12994f3aecf66ac22cd5e6df9a723d7d264f0a4f
Amin Bandali <amin@aminb.org>
2017-09-08 12:11:41 -0400

    Add `:private haskell` based on older `:lang haskell`
    With a few small changes.
    
    TODO: look into updating this private module to match the current
    lang module.

---
+++ modules/private/haskell/packages.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; -*-
+;;; private/haskell/packages.el
+
+(package! haskell-mode)
+;; (package! dante)
+
+;; (when (featurep! :completion company)
+;;   (package! company-ghc))
